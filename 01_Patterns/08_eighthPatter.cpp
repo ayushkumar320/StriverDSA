3eef27652997f579511c15c2f6fb28db92dda532
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-08-15 09:57:42 +0530

    eighth pattern

---
+++ 01_Patterns/08_eighthPatter.cpp
@@ -0,0 +1,35 @@
+/*
+
+*********
+ *******
+  *****
+   ***
+    *
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void printPattern(int n){
+  for(int i = 0; i < n; i++){
+    // Space
+    for(int j = n; j > n - i; j--){
+      cout << " ";
+    }
+    // Star
+    for(int j = 2*(n-i) - 1; j > 0; j--){
+      cout << '*';
+    }
+    // Space
+    for(int j = n; j > n - i; j--){
+      cout << " ";
+    }
+    cout << endl;
+  }
+}
+
+int main(){
+  printPattern(5);
+  return 0;
+}
