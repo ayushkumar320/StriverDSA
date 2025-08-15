98cfdb3962947f49b494347c58f2a7a08034fddb
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-08-15 09:05:15 +0530

    extracted the logic to a function

--- 01_Patterns/01_firstPatter.cpp
+++ 01_Patterns/01_firstPatter.cpp
@@ -10,14 +10,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-  
-  for(int i = 0 ; i < 4; i++){
-    for (int j = 0; j < 4; j++){
+void printPattern(int n){
+  for(int i = 0 ; i < n; i++){
+    for (int j = 0; j < n; j++){
       cout << '*' <<" ";
     }
     cout << endl;
   }
+}
+int main(){
+  
+  printPattern(4);
   
   return 0;
 }
