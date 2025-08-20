67d8e2d254aa1f432d876ddaea21cf4d09c3cd6d
Ayush <ayushkumar85385@gmail.com>
2025-08-20 12:09:11 +0530

    bubble sort

--- 06_Sorting/02_bubbleSort.cpp
+++ 06_Sorting/02_bubbleSort.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 
 void bubble_sort(int arr[], int n) {
+  int didSwap = 0;
   for(int i = n-1; i >= 0; i--) {
     for(int j = 0; j <= i-1; j++) {
       if (arr[j] > arr[j+1]) {
         swap(arr[j], arr[j+1]);
+        didSwap = 1;
       }
+    }
+    if (didSwap == 0) {
+      break;
     }
   }
 }
